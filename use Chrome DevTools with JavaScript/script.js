88e5f4856d516f0364be61fbe421b7b081da56f1
Farid Attamimi <97972332+Tamimi2022@users.noreply.github.com>
2022-03-30 17:59:21 +0200

    Add files via upload

---
+++ use Chrome DevTools with JavaScript/script.js
@@ -0,0 +1,10 @@
+// Part 1
+document.body.innerHTML = "I'm a new paragraph";
+"I'm a new paragraph"
+
+
+// Part 2
+const paragraphElement = document.getElementById("first-section");
+console.log(paragraphElement);
+
+// Part 3
